cb9adcd218e3109e571d91db51dda3165618181f
Nicholas Henry <nicholas.henry@gmail.com>
2013-09-10 15:32:10 -0400

    Restrict Mime-type to JSON

--- app/controllers/spell_checker_controller.rb
+++ app/controllers/spell_checker_controller.rb
@@ -1,8 +1,13 @@
 class SpellCheckerController < ApplicationController
 
+  include ActionController::MimeResponds
+
   def check
     result = spell_check(params[:q])
-    render json: result.to_json
+
+    respond_to do |format|
+      format.json { render json: result.to_json }
+    end
 
   rescue Wordstock::SpellChecker::BadWord => e
     render json: format_messages(e.message), status: :bad_request
