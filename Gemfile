cb901cc824c340fbdd9ee8695dda07c872398e95
Nicholas Henry <nicholas.henry@gmail.com>
2013-09-10 10:11:03 -0400

    Remove insecure default of storing secret token in repo
    Store sensitive settings in environment file. The token move into the example environment file should not be used in production.

--- Gemfile
+++ Gemfile
@@ -3,9 +3,8 @@ source 'https://rubygems.org'
 # Bundle edge Rails instead: gem 'rails', github: 'rails/rails'
 gem 'rails', '4.0.0'
 
+gem 'dotenv-rails'
 gem 'rails-api'
-
-
 
 # To use ActiveModel has_secure_password
 # gem 'bcrypt-ruby', '~> 3.0.0'
--- config/initializers/secret_token.rb
+++ config/initializers/secret_token.rb
@@ -10,10 +10,10 @@
 # Make sure your secret_key_base is kept private
 # if you're sharing your code publicly.
 
-# Although this is not needed for an api-only application, rails4 
-# requires secret_key_base or secret_toke to be defined, otherwise an 
+# Although this is not needed for an api-only application, rails4
+# requires secret_key_base or secret_toke to be defined, otherwise an
 # error is raised.
 # Using secret_token for rails3 compatibility. Change to secret_key_base
 # to avoid deprecation warning.
 # Can be safely removed in a rails3 api-only application.
-Wordstock::Application.config.secret_token = 'b499c0f4699912d7326e312c35780d1c7cecfaadf9ef9f110ea6f52c1e390851cbf49c5e807acefba4a248885037e26a0aaa6e7ace64adf277a83be97cdffd53'
+Wordstock::Application.config.secret_key_base = ENV['RAILS_SECRET_KEY_BASE']
