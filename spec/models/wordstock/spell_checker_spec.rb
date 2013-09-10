a25af1a3ccecbc0b88a88c9accbeb355aed30702
Nicholas Henry <nicholas.henry@gmail.com>
2013-09-10 14:59:30 -0400

    Add spec for SpellChecker model before integrating 3rd party library

--- spec/models/wordstock/spell_checker_spec.rb
+++ spec/models/wordstock/spell_checker_spec.rb
@@ -8,5 +8,43 @@ module Wordstock
         expect{ SpellChecker.check(nil) }.to raise_error(Wordstock::SpellChecker::BadWord)
       end
     end
+
+    context "given a correctly spelled word" do
+      let(:actual) {  SpellChecker.check("scintillating") }
+
+      it "returns the original word" do
+        expect(actual[:original]).to eq("scintillating")
+      end
+
+      it "flags the word as correct" do
+        expect(actual[:correct]).to be_true
+      end
+
+      it "returns no suggestions" do
+        expect(actual[:suggestions]).to be_nil
+      end
+    end
+
+    context "given an incorrectly spelled word" do
+      let(:actual) {  SpellChecker.check("scintillatingZ") }
+
+      it "returns the original word" do
+        expect(actual[:original]).to eq("scintillatingZ")
+      end
+
+      it "flags the word as incorrect" do
+        expect(actual[:correct]).to be_false
+      end
+
+      it "returns no suggestions" do
+        expect(actual[:suggestions]).to eq(
+          ["scintillating",
+           "scintillation's",
+           "scintillation",
+           "scintillates",
+           "scintillate"]
+        )
+      end
+    end
   end
 end
